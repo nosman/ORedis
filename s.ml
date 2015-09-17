c04273822aee1f8b8278ac394c572665d0fcfef7
stephanos <sgt29@cornell.edu>
2015-09-16 23:26:28 -0700

    Key-related functions are in the api sig.

--- s.ml
+++ s.ml
@@ -28,4 +28,51 @@ module type Api = sig
 	val parse_resp : I.reader -> resp_response I.t
 
 	val send_command : I.writer * I.reader -> string -> string list -> resp_response I.t
+
+	val del : I.writer * I.reader -> string list -> int I.t
+
+	val dump : I.writer * I.reader -> string -> string I.t
+
+	val exists : I.writer * I.reader -> string -> bool I.t
+
+	val expire : I.writer * I.reader -> string -> int -> bool I.t
+
+	val expireat : I.writer * I.reader -> string -> float -> bool I.t
+
+	val keys : I.writer * I.reader -> string -> string list I.t
+
+	val migrate : I.writer * I.reader -> string -> int -> string -> string -> ?copy:bool -> ?replace:bool -> int -> string I.t
+
+	val move : I.writer * I.reader -> string -> string -> string I.t
+
+	val object_refcount : I.writer * I.reader -> string -> int option I.t
+
+	val object_idletime : I.writer * I.reader -> string -> int option I.t
+
+	val object_encoding : I.writer * I.reader -> string -> string option I.t
+
+	val persist : I.writer * I.reader -> string -> bool I.t
+
+	val pexpire : I.writer * I.reader -> string -> int -> bool I.t
+
+	val pexpireat : I.writer * I.reader -> string -> int -> bool I.t
+
+	val pttl : I.writer * I.reader -> string -> int I.t
+
+	val randomkey : I.writer * I.reader -> string option I.t
+
+	val rename : I.writer * I.reader -> string -> string -> string I.t
+
+	val renamenx : I.writer * I.reader -> string -> string -> bool I.t
+
+	val restore : I.writer * I.reader -> ?replace:bool -> string -> int -> string -> string I.t
+
+	val sort :
+	  I.writer * I.reader ->
+	  ?by:string -> ?limit:int * int -> ?get:'a list -> ?order:[< `Asc | `Desc ] -> ?alpha:bool -> 'b -> string list I.t
+
+	val ttl : I.writer * I.reader -> string -> int I.t
+
+	val type_ : I.writer * I.reader -> string -> string I.t
+
 end
